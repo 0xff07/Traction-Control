3890b3d02f65f7b2be585e77725e78acfe63b68a
0xff <0xff@0xff.local>
2017-04-27 10:58:31 +0800

    Syntax tweaks
    Add comments and remove unused variables.

--- MidtermProject.ino
+++ MidtermProject.ino
@@ -1,22 +1,30 @@
 #include <Servo.h>
+
+/* pin for TCS3200 */
 const int s0 = 8;  
 const int s1 = 9;  
 const int s2 = 12;  
 const int s3 = 11;  
 const int out = 10;   
-// LED pins connected to Arduino
-int redLed = 2;  
-int greenLed = 3;  
-int blueLed = 4;
-// Variables  
+
+/* RGB values */
 int red = 0;  
 int green = 0;  
 int blue = 0;  
-int NORMAL[3] = {28, 23, 15};
-int BLACK[3] = {40, 35, 25};
 
+/* Ambient and target(Black) color value */
+const int NORMAL[3] = {28, 23, 15};
+const int BLACK[3] = {40, 35, 25};
+
+/* Pin for ESC */
 byte servoPin = 6;
 Servo servo;
+
+/* pin for IR tracking sensor */
+int sensorPin = A0;
+
+/* variables for IR tracking sensor */
+int sensorValue = 0;
 
 void setup()   
 {  
@@ -27,14 +35,10 @@ void setup()
   pinMode(s2, OUTPUT);  
   pinMode(s3, OUTPUT);  
   pinMode(out, INPUT);  
-  pinMode(redLed, OUTPUT);  
-  pinMode(greenLed, OUTPUT);  
-  pinMode(blueLed, OUTPUT);  
   digitalWrite(s0, HIGH);  
   digitalWrite(s1, HIGH);
 
   /* set ESC */
-
   servo.attach(servoPin);
   servo.writeMicroseconds(1200); // send "stop" signal to ESC.
   delay(1000); // delay to allow the ESC to recognize the stopped signal  servo.attach(servoPin);
@@ -44,11 +48,11 @@ void setup()
 void loop() 
 {  
   color(); 
-  Serial.print("R Intensity:");  
+  Serial.print("R:");  
   Serial.print(red, DEC);  
-  Serial.print(" G Intensity: ");  
+  Serial.print(" G: ");  
   Serial.print(green, DEC);  
-  Serial.print(" B Intensity : ");  
+  Serial.print(" B: ");  
   Serial.print(blue, DEC);  
   //Serial.println();  
 
@@ -56,6 +60,8 @@ void loop()
   int signal = 1200; // Set signal value, which should be between 1100 and 1900
   servo.writeMicroseconds(signal); // Send signal to ESC.
   */
+  sensorValue = analogRead (sensorPin);
+  Serial.println (sensorValue, DEC); //debug
   
   if(isBlack(red, green, blue)){
     Serial.println("- (Black Color)");
@@ -71,16 +77,27 @@ void loop()
 
     
 void color()  
-{    
+{ 
+  digitalWrite(s0, LOW);  
+  digitalWrite(s1, HIGH);
+  
+  /* set TSC3200 to read RED */
+  /* (s2, s3) = (LOW, LOW)   */   
   digitalWrite(s2, LOW);  
   digitalWrite(s3, LOW);  
-  //count OUT, pRed, RED  
-  red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);  
+  /* count OUT, pRed, RED    */  
+  red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+
+  /* set TCS3200 to read BLUE */
+  /* (s2, s3) = (LOW, HIGH)   */
   digitalWrite(s3, HIGH);  
-  //count OUT, pBLUE, BLUE  
-  blue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);  
+  /* count OUT, pBLUE, BLUE   */  
+  blue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH); 
+
+  /* set TCS3200 to read GREEN */
+  /* (s2, s3) = (HIGH, HIGH)   */
   digitalWrite(s2, HIGH);  
-  //count OUT, pGreen, GREEN  
+  /* count OUT, pGreen, GREEN  */  
   green = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);  
 }
 
@@ -90,6 +107,4 @@ int isBlack(int R, int G, int B) {
     return 0;
 }
 
-int isNormal(int R, int G, int B) {
-    
-}
+
